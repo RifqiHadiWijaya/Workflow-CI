c256210fd5ac2b8405c5b6c766a99e9dbec7afd7
RifqiHadiWijaya <rifqihadiwijaya2005@gmail.com>
2025-12-22 23:42:41 +0800

    setup kriteria 3

---
+++ MLProject/modelling.py
@@ -0,0 +1,37 @@
+import mlflow
+import mlflow.sklearn
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+# Set experiment
+mlflow.set_experiment("Fraud_No_Tuning")
+
+# Autolog
+mlflow.autolog()
+
+df = pd.read_csv("data/processed/train_fraud.csv")
+
+X = df.drop(columns=["Fraud_Label"])
+y = df["Fraud_Label"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, stratify=y, random_state=42
+)
+
+with mlflow.start_run():
+    model = RandomForestClassifier(
+        n_estimators=100,
+        class_weight="balanced",
+        random_state=42
+    )
+    model.fit(X_train, y_train)
+
+    preds = model.predict(X_test)
+    acc = accuracy_score(y_test, preds)
+
+    mlflow.log_metric("accuracy", acc)
+    mlflow.sklearn.log_model(model, "model")
+
+    print("Accuracy:", acc)
